e44b786731649d4b9e2dc349496d7903ad876125
wrightwells <wrightwells@hotmail.com>
2024-03-27 20:00:53 +0000

    Update 01GtSt.lua reorder screen

--- TELEMETRY/01GtSt/01GtSt.lua
+++ TELEMETRY/01GtSt/01GtSt.lua
@@ -34,9 +34,11 @@ local BatterySensor = "RxBt"
 local ReceiverBatteryVoltage_current = 0.0
 local ReceiverBatteryVoltage_previous = 0.0
 local ReceiverBatteryVoltage_startup = 0.0
+local MaxBatteryVoltage = 4.2
+local MinBatteryVoltage = 3.4
 local x = 0
 local y = 0
-local fh = 10
+local fh = 20
 
 local function init_func()
   -- Called once when model is loaded
@@ -67,37 +69,8 @@ local function run_func(event)
 
   -- LCD / Display code
   lcd.clear()
-
-  -- lcd.drawChannel(x, y, source, flags)
-  -- Displays sensor value with units
-  -- x,y location with 0,0 left, top
-  -- source can only be telemetry sensors
-  -- flags are optional
-  -- 0 black on white text, 1 blink,
-  -- 2 white on black text, 3 blink
-  x = 1
+  x = 2
   y = 1
-  if ReceiverBatteryVoltage_current == ReceiverBatteryVoltage_startup then
-    lcd.drawChannel(x,y, BatterySensor)
-  else
-    lcd.drawChannel(x,y, BatterySensor, BLINK)
-  end
-  -- lcd.getLastPos()
-  -- Returns the last pixel drawn position from the left
-  lcd.drawText( lcd.getLastPos(), y, " "..BatterySensor.." Sensor" )
-
-  -- lcd.drawGauge(x, y, w, h, fill, maxfill)
-  -- Draws a horizontal gauge, fills from left to right
-  -- x,y location with 0,0 left, top
-  -- w,h width and height in pixels
-  -- fill is the current value (example 20 for 20%)
-  -- maxfill is the max gauge value (example 100 for % scale)
-  y = y + fh
-  if ReceiverBatteryVoltage_startup > 0 then
-    lcd.drawGauge(x, y, LCD_W-1, 2 * fh, ReceiverBatteryVoltage_current*10, ReceiverBatteryVoltage_startup*10)
-  else
-    lcd.drawText (x,y, "Rx or Simulator not on")
-  end
   -- lcd.drawNumber(x, y, value [, flags])
   -- Display numbers
   -- x,y location with 0,0 left, top
@@ -109,8 +82,41 @@ local function run_func(event)
   -- PREC2 is value/100 so 123 becomes 1.23
   -- if the value is 5.2 then 2 gets dropped
   -- to display 5.2, value x 10 PREC1
-  lcd.drawNumber(1, 40,  ReceiverBatteryVoltage_startup *10, PREC1)
-  lcd.drawText( lcd.getLastPos(), 40, "V at start up.")
+  lcd.drawNumber(x, y,  ReceiverBatteryVoltage_startup *10, PREC1)
+  lcd.drawText( lcd.getLastPos(), y, "V at start up.")
+
+
+ 
+  -- lcd.drawGauge(x, y, w, h, fill, maxfill)
+  -- Draws a horizontal gauge, fills from left to right
+  -- x,y location with 0,0 left, top
+  -- w,h width and height in pixels
+  -- fill is the current value (example 20 for 20%)
+  -- maxfill is the max gauge value (example 100 for % scale)
+  y = y + fh
+  if ReceiverBatteryVoltage_startup > 0 then
+    lcd.drawGauge(x, y, LCD_W-1, 2 * fh, ReceiverBatteryVoltage_current*10, MaxBatteryVoltage*10)
+    --lcd.drawGauge(x, y, LCD_W-1, 2 * fh, ReceiverBatteryVoltage_current*10, ReceiverBatteryVoltage_startup*10)
+  else
+    lcd.drawText (x,y, "Quadcopter not on")
+  end
+   -- lcd.drawChannel(x, y, source, flags)
+  -- Displays sensor value with units
+  -- x,y location with 0,0 left, top
+  -- source can only be telemetry sensors
+  -- flags are optional
+  -- 0 black on white text, 1 blink,
+  -- 2 white on black text, 3 blink
+  
+  --if ReceiverBatteryVoltage_current == ReceiverBatteryVoltage_startup then
+  lcd.drawChannel(x,y, BatterySensor, XXLSIZE)
+  -- else
+   --  lcd.drawChannel(x,y, BatterySensor, BLINK)
+  -- end
+   -- lcd.getLastPos()
+   -- Returns the last pixel drawn position from the left
+   lcd.drawText( lcd.getLastPos(), y, " "..BatterySensor.." Sensor" )
+ 
 end
 
 
